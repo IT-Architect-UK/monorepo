aaccc395fa556791b1acda74480865274a986642
Pilkie <39397979+DazPilkie@users.noreply.github.com>
2025-08-27 16:01:21 +0100

    Latest Update

--- scripts/bash/ubuntu/vm-templates/customization/alma-vm-template-prepare.sh
+++ scripts/bash/ubuntu/vm-templates/customization/alma-vm-template-prepare.sh
@@ -27,8 +27,22 @@ sudo rm -f /etc/NetworkManager/system-connections/*
 sudo truncate -s 0 /etc/hostname
 sudo hostnamectl set-hostname ""
 
-# Reset machine ID
-sudo rm -f /etc/machine-id
+# Disable IPv6
+# Add GRUB parameters
+sudo sed -i 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="ipv6.disable=1 /' /etc/default/grub
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# Set sysctl to disable IPv6
+echo -e "net.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1" | sudo tee /etc/sysctl.d/99-disable-ipv6.conf
+sudo sysctl -p /etc/sysctl.d/99-disable-ipv6.conf
+
+# Disable IPv6 in NetworkManager
+sudo nmcli connection modify "$(nmcli -t -f UUID,TYPE connection show | grep ethernet | cut -d: -f1)" ipv6.method disabled
+sudo systemctl restart NetworkManager
+
+# Clear machine ID (safely)
+sudo truncate -s 0 /etc/machine-id
+sudo touch /etc/machine-id
 
 # Clear logs
 sudo truncate -s 0 /var/log/*.log
@@ -48,6 +62,15 @@ sudo sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/s
 sudo sed -i 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
 sudo sshd -t
 sudo systemctl restart sshd
+
+# Ensure dbus-broker starts after dependencies
+sudo sed -i '/^After=/ s/$/ network.target systemd-udev.service/' /usr/lib/systemd/system/dbus-broker.service
+sudo systemctl daemon-reload
+
+# Disable cloud-init to prevent loops
+sudo systemctl disable cloud-init cloud-init-local --now
+sudo rm -rf /var/lib/cloud/*
+sudo rm -f /var/log/cloud-init*
 
 # Shutdown the system
 sudo systemctl poweroff
